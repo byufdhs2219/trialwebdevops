09d7d925c2c94f106ae9f9583116036f97bb9f3a
Bayu Fadhlurrohman Hasmi <byufd171@gmail.com>
2025-12-26 10:44:12 +0700

    first-commit

---
+++ app/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+require('dotenv').config();
+
+const logger = require('./middleware/logger');
+app.use(logger);
+
+
+// Dashboard
+// app.use('/dashboard', express.static(path.join(__dirname, 'public', 'dashboard')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
